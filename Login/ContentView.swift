8bf762ee2f4a892e3fec932c7ad074a91e93a9f4
Deepak2909 <sdyadav200@gmail.com>
2021-07-11 18:19:36 +0530

    Initial Commit

---
+++ Login/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Login
+//
+//  Created by Deepak on 11/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Login/LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginApp.swift
+//  Login
+//
+//  Created by Deepak on 11/07/21.
+//
+
+import SwiftUI
+
+@main
+struct LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
